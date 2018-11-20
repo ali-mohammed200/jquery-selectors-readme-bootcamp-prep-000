a65df6b7f6392df1154a3a54e12253f136b055cd
Mohammad Ali <ali.mohammed200@gmail.com>
2018-11-20 06:45:48 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,11 +5,10 @@ function paragraphSelector(){
 function lastImageSelector(){
   return $('img:last')
 }
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
 /*
-+ Write a function `lastImageSelector` that does not accept any parameters. The
-function should use the last jQuery selector to return the last image in
-`index.html`.
-
 + Write a function `ninjaBabySelector` that does not accept any parameters. The
 function should use an ID selector to return the ninja baby image.
 
